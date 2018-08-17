9bba6bdf5eecb926bc433a55720ea9e1a223b962
Erik Rose <erik@mozilla.com>
2018-08-17 14:56:01 -0400

    Get rid of a CommonJS-style `module.exports` reference that somehow survived. Ref #81.

--- utilsForBackend.mjs
+++ utilsForBackend.mjs
@@ -8,7 +8,7 @@ import {jsdom} from 'jsdom/lib/old-api';
  * @return {String[]} The name (not path) of each directory directly within a
  *      given path
  */
-function dirsIn(path) {
+export function dirsIn(path) {
     return readdirSync(path).filter(f => statSync(join(path, f)).isDirectory());
 }
 
@@ -16,12 +16,7 @@ function dirsIn(path) {
  * Parse an HTML doc, and return a DOM-compliant interface to it. Do not
  * execute any of its inline scripts.
  */
-function staticDom(html) {
+export function staticDom(html) {
     return jsdom(html, {features: {ProcessExternalResources: false,
                                    FetchExternalResources: false}});
 }
-
-module.exports = {
-    dirsIn,
-    staticDom
-};
