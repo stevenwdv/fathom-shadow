cbad4cacc37c7d275cc1285c802d8c3feb0d5e69
Erik Rose <erik@mozilla.com>
2018-10-09 14:58:13 -0400

    Support pluggable success functions in fathom-trainees.
    Glob did most of this work. Close https://github.com/mozilla/fathom-trainees/pull/2.
    Fix https://github.com/mozilla/fathom-fox/issues/18.
    Fix https://github.com/mozilla/fathom-fox/issues/7.

--- trainees.js
+++ trainees.js
@@ -30,6 +30,16 @@ export function initBackgroundScript() {
     browser.runtime.onMessageExternal.addListener(handleExternalMessage);
 }
 
+/**
+ * The default success function for a ruleset: succeed if the found element has
+ * a data-fathom attribute equal to the traineeId. We arbitrarily use the first
+ * found node if multiple are found.
+ */
+function foundLabelIsTraineeId(facts, traineeId) {
+    const found = facts.get(traineeId);
+    return found.length ? found[0].element.dataset.fathom === traineeId : false;
+}
+
 /** React to commands sent from the background script. */
 async function dispatch(request) {
     switch (request.type) {
@@ -37,21 +47,11 @@ async function dispatch(request) {
             // Run the trainee ruleset of the given ID with the given coeffs
             // over the document, and report whether it found the right
             // element.
-            const rules = trainees.get(request.traineeId).rulesetMaker(request.coeffs);
+            const trainee = trainees.get(request.traineeId);
+            const rules = trainee.rulesetMaker(request.coeffs);
             const facts = rules.against(window.document);
-            // Assume the out() key and the data-fathom attr are both identical
-            // to the key of the trainee in the map.
-            const found = facts.get(request.traineeId);
-            if (found.length >= 1) {
-                const fnode = found[0];  // arbitrary pick
-                if (fnode.element.getAttribute('data-fathom') === request.traineeId) {
-                    return true;
-                }
-                //console.log(urlFilename(window.location.href), ": found wrong answer class=", fnode.element.getAttribute('class'), 'id=', fnode.element.getAttribute('id'));
-            } else {
-                //console.log(urlFilename(window.location.href), ": found nothing.");
-            }
-            return false;
+            const successFunc = trainee.successFunction || foundLabelIsTraineeId;
+            return successFunc(facts, request.traineeId);
             break;  // belt, suspenders
     }
     return Promise.resolve({});
