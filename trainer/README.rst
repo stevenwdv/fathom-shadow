830dfdcc7c4fc7d4e83b3ba30c0d750bc9d53c94
Erik Rose <erik@mozilla.com>
2019-03-29 16:50:21 -0400

    Add commandline trainer that uses PyTorch.
    Integration into the main docs is coming. For now, `pip install fathom-web`, then run `fathom-train --help`.

---
+++ trainer/README.rst
@@ -0,0 +1,11 @@
+==================
+The Fathom Trainer
+==================
+
+This is the commandline trainer for `Fathom <https://mozilla.github.io/fathom/>`_, which itself is a supervised-learning system for recognizing parts of web pages.
+
+Version History
+===============
+
+3.0a1
+  * First release, intended for use with Fathom itself 3.0 or later
---
+++ trainer/setup.py
@@ -0,0 +1,25 @@
+from io import open
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fathom-web',
+    version='3.0a1',
+    description='Commandline tools for training Fathom rulesets',
+    long_description=open('README.rst', 'r', encoding='utf8').read(),
+    author='Erik Rose',
+    author_email='erik@mozilla.com',
+    license='MPL',
+    packages=find_packages(),
+    url='https://mozilla.github.io/fathom/',
+    install_requires=['click>=7.0,<8.0', 'tensorboardX>=1.6,<2.0', 'torch>=1.0,<2.0'],
+    entry_points={'console_scripts': ['fathom-train = fathom_web:train']},
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 3'
+        ],
+    keywords=['machine learning', 'ml', 'semantic extraction'],
+)
