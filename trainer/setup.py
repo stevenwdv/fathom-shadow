9d75b687ef28aa9960e26692bbe00757867eece1
Erik Rose <erik@mozilla.com>
2019-04-01 10:14:28 -0400

    Move fathom-train out of `__init__` to make room for other commandline utils.

--- trainer/setup.py
+++ trainer/setup.py
@@ -13,7 +13,9 @@ setup(
     packages=find_packages(),
     url='https://mozilla.github.io/fathom/',
     install_requires=['click>=7.0,<8.0', 'tensorboardX>=1.6,<2.0', 'torch>=1.0,<2.0'],
-    entry_points={'console_scripts': ['fathom-train = fathom_web:train']},
+    entry_points={'console_scripts': [
+        'fathom-train = fathom_web.train:main',
+    ]},
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
