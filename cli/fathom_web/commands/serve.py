5cf2e902d3cdd01368ed72e7796b848f741e17b0
Daniel Hertenstetin <daniel.hertenstein@gmail.com>
2019-07-26 15:21:00 -0400

    Introduces a context manager to change directory since `SimpleHTTPRequestHandler` didn't add its `directory` parameter until Python 3.7

--- cli/fathom_web/commands/serve.py
+++ cli/fathom_web/commands/serve.py
@@ -1,4 +1,4 @@
-from functools import partial
+import contextlib
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
 from pkg_resources import resource_filename
@@ -19,14 +19,26 @@ def main(directory, port):
     This is useful for vectorizing samples using FathomFox. FathomFox expects you to provide,
     in the vectorizer page, an address to an HTTPS server that is serving your samples.
     """
-    handler = partial(SimpleHTTPRequestHandler, directory=directory)
-    server = HTTPServer(('localhost', port), handler)
-    certfile = resource_filename('fathom_web', 'cert.pem')
-    server.socket = ssl.wrap_socket(
-        server.socket,
-        certfile=certfile,
-        server_side=True,
-    )
-    print(f'Serving {directory} over https://localhost:{port}.')
-    print('Press Ctrl+C to stop.')
-    server.serve_forever()
+    with cd(directory):
+        server = HTTPServer(('localhost', port), SimpleHTTPRequestHandler)
+        certfile = resource_filename('fathom_web', 'cert.pem')
+        server.socket = ssl.wrap_socket(
+            server.socket,
+            certfile=certfile,
+            server_side=True,
+        )
+        print(f'Serving {directory} over https://localhost:{port}.')
+        print('We are using a self-signed certificate, so your browser will warn you about'
+              ' a potential security risk when opening the first sample. This is safe.')
+        print('Press Ctrl+C to stop.')
+        server.serve_forever()
+
+
+@contextlib.contextmanager
+def cd(path):
+    previous_directory = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(previous_directory)
