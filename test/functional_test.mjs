43e4a30aa3ec077e9c37579ec3a89f302cc004ee
Bianca Danforth <biancadanforth@gmail.com>
2019-08-05 15:44:16 -0700

    Test Fathom's isVisible method on a page

--- test/functional_test.mjs
+++ test/functional_test.mjs
@@ -1,33 +1,34 @@
 const { expect } = require('chai');
 const firefox = require('selenium-webdriver/firefox');
-const webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until,
-    Key = webdriver.Key;
+const webdriver = require('selenium-webdriver');
+const { ancestors, isDomElement, isVisible, toDomElement } = require('../utilsForFrontend');
 
 describe('isVisible', () => {
     const options = new firefox.Options();
-    // TODO: also add options.setBinary('path/to/binary'); get from englehardt
     options.headless();
 
     const driver = new webdriver.Builder()
-        // TODO: can set version and platform in forBrowser
         .forBrowser('firefox')
-        // .setFirefoxOptions(options)
+        .setFirefoxOptions(options)
         .build();
     describe('Unprivileged', () => {
-        it('should return false when...TODO', async () => {
+        it('should return true when an element is visible', async () => {
             // TODO: put actual checks here
             await driver.get('https://developer.mozilla.org/');
-            await driver.findElement(By.id('home-q')).sendKeys('testing', Key.RETURN);
-            await driver.wait(until.titleIs('Search Results for "testing" | MDN'));
             await driver.wait(async () => {
                 const readyState = await driver.executeScript('return document.readyState');
                 return readyState === 'complete';
             });
-            const input = 2;
-            const expected = 2;
-            expect(input).to.deep.equal(expected);
+            const isElementVisible = await driver.wait(async () => {
+                return driver.executeScript(`
+                    ${ancestors}
+                    ${isDomElement}
+                    ${toDomElement}
+                    return (${isVisible}(document.getElementById("home-q")));
+                `);
+            });
+            const expected = true;
+            expect(isElementVisible).to.deep.equal(expected);
         });
     });
 
