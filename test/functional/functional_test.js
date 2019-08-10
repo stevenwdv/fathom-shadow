938234ef9219eaa1206b4d35f6f8cde70bbf8f73
Bianca Danforth <biancadanforth@gmail.com>
2019-08-09 21:15:59 -0700

    Update test cases

--- test/functional/functional_test.js
+++ test/functional/functional_test.js
@@ -3,6 +3,9 @@ const firefox = require('selenium-webdriver/firefox');
 const webdriver = require('selenium-webdriver');
 const { ancestors, isDomElement, isVisible, toDomElement } = require('../../utilsForFrontend');
 
+const WAIT_MS = 10000;
+const TEST_PAGE_URL = 'http://localhost:8000/functional_test.html';
+
 describe('isVisible', () => {
     const options = new firefox.Options();
     options.headless();
@@ -11,24 +14,41 @@ describe('isVisible', () => {
         .forBrowser('firefox')
         .setFirefoxOptions(options)
         .build();
+
     describe('Unprivileged', () => {
-        it('should return true when an element is visible', async () => {
-            // TODO: put actual checks here
-            await driver.get('http://localhost:8000/functional_test.html');
+        it('Should return false when an element is hidden', async () => {
+            const hiddenElementIds = [
+                'not-visible-1',
+                'not-visible-2',
+                'not-visible-3',
+                'not-visible-4',
+                'not-visible-5',
+                'not-visible-6',
+                'not-visible-7',
+                'not-visible-8',
+                'not-visible-9',
+                'not-visible-10',
+                'not-visible-11',
+                'not-visible-12',
+            ];
+            await driver.get(TEST_PAGE_URL);
             await driver.wait(async () => {
                 const readyState = await driver.executeScript('return document.readyState');
                 return readyState === 'complete';
-            });
-            const isElementVisible = await driver.wait(async () => {
-                return driver.executeScript(`
+            }, WAIT_MS, `Page did not finish loading after ${WAIT_MS} ms`);
+
+            for (const id of hiddenElementIds) {
+                const isElementVisible = await driver.executeScript(`
                     ${ancestors}
                     ${isDomElement}
                     ${toDomElement}
-                    return (${isVisible}(document.getElementById("image")));
+                    return ${isVisible}(document.getElementById('${id}'));
                 `);
-            });
-            const expected = true;
-            expect(isElementVisible).to.deep.equal(expected);
+                expect(
+                    isElementVisible,
+                    `isVisible should return false for element with id '${id}'.`
+                ).to.be.false;
+            }
         });
     });
 
