b04d519e9828dcde74ac99215af80d9274c5f2b5
Bianca Danforth <biancadanforth@gmail.com>
2019-08-19 21:13:39 -0700

    Incorporate other review feedback

--- test/browser/isVisible.js
+++ test/browser/isVisible.js
@@ -1,9 +1,7 @@
 const {assert} = require('chai');
 const firefox = require('selenium-webdriver/firefox');
-const webdriver = require('selenium-webdriver');
+const {Builder, until, By} = require('selenium-webdriver');
 const {ancestors, isDomElement, isVisible, toDomElement} = require('../../utilsForFrontend'); // eslint-disable-line node/no-missing-require
-
-const {Builder, until, By} = webdriver;
 
 const WAIT_MS = 10000;
 const TEST_PAGE_URL = 'http://localhost:8000/isVisible.html';
@@ -17,7 +15,7 @@ describe('isVisible', () => {
         .setFirefoxOptions(options)
         .build();
 
-    async function checkVisibility(id, expected) {
+    async function checkElementVisibility(id, expected) {
         await driver.wait(until.elementLocated(By.id(id)), WAIT_MS);
         const isElementVisible = await driver.executeScript(`
             ${ancestors}
@@ -32,30 +30,26 @@ describe('isVisible', () => {
         );
     }
 
-    it('should return false when an element is hidden', async function () {
-        this.timeout(WAIT_MS);
-        const hiddenElementIds = await driver.executeScript(`
-            return [].slice.call(document.querySelectorAll('[id^="not-visible-"]')).map((element) => element.id);
+    async function checkElementsVisibility(idStub, isVisible) {
+        const elementIds = await driver.executeScript(`
+            return Array.prototype.map.call(document.querySelectorAll('[id^="${idStub}"]'), (element) => element.id);
         `);
 
         await driver.get(TEST_PAGE_URL);
 
-        for (const id of hiddenElementIds) {
-            await checkVisibility(id, false);
-
+        for (const id of elementIds) {
+            await checkElementVisibility(id, isVisible);
         }
+    }
+
+    it('should return false when an element is hidden', async function () {
+        this.timeout(WAIT_MS);
+        await checkElementsVisibility('not-visible-', false);
     });
 
     it('should return true when an element is visible', async function () {
         this.timeout(WAIT_MS);
-        const visibleElementIds = await driver.executeScript(`
-            return [].slice.call(document.querySelectorAll('[id^="visible-"]')).map((element) => element.id);
-        `);
-        await driver.get(TEST_PAGE_URL);
-
-        for (const id of visibleElementIds) {
-            await checkVisibility(id, true);
-        }
+        await checkElementsVisibility('visible-', true);
     });
 
     after(async function () {
