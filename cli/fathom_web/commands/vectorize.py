923ec3e1f7b86aed3484ae14b0be280d7395c985
Daniel Hertenstetin <daniel.hertenstein@gmail.com>
2019-11-04 13:21:17 -0500

    Load fathom addons

--- cli/fathom_web/commands/vectorize.py
+++ cli/fathom_web/commands/vectorize.py
@@ -1,4 +1,6 @@
+import pathlib
 import subprocess
+import zipfile
 
 from selenium import webdriver
 
@@ -6,16 +8,14 @@ from selenium import webdriver
 def main(trainees_file, samples_directory, output_directory, headless_browser):
     sample_filenames = run_fathom_list(samples_directory)
 
-#   need_to_vectorize = check_for_changes_to_samples_or_ruleset(sample_filenames, trainees_file)
+    # TODO: Delete this!!!
     need_to_vectorize = True
+#   need_to_vectorize = check_for_changes_to_samples_or_ruleset(sample_filenames, trainees_file)
     if not need_to_vectorize:
         vector_file = use_previous_vectors()
     else:
-#       fathom_fox, fathom_trainees = build_fathom_addons(trainees_file)
+        fathom_fox, fathom_trainees = build_fathom_addons(trainees_file)
         file_server = set_up_file_server(samples_directory)
-        # TODO: Delete this!!!
-        fathom_fox = None
-        fathom_trainees = None
         firefox = configure_firefox(fathom_fox, fathom_trainees, headless_browser)
 #       firefox = set_up_vectorizer(firefox, sample_filenames)
         vector_file, firefox = run_vectorizer(firefox)
@@ -38,10 +38,21 @@ def use_previous_vectors():
 #   return vector_file
 
 
-#def build_fathom_addons(trainees_file):
-#    return fathom_fox, fathom_trainees
-#
-#
+def build_fathom_addons(trainees_file):
+    fathom_fox = create_xpi_for(pathlib.Path('C:/Users/Daniel/code/fathom-fox/addon'), 'fathom-fox')
+    # TODO: Copy the trainees file into the trainees repo
+    fathom_trainees = create_xpi_for(pathlib.Path('C:/Users/Daniel/code/fathom-trainees/addon'), 'fathom-trainees')
+    return fathom_fox, fathom_trainees
+
+
+def create_xpi_for(directory, name):
+    xpi_path = pathlib.Path(f'{name}.xpi')
+    with zipfile.ZipFile(xpi_path, 'w', compression=zipfile.ZIP_DEFLATED) as xpi:
+        for file in directory.rglob('*'):
+            xpi.write(file, file.relative_to(directory))
+    return str(xpi_path.absolute())
+
+
 def set_up_file_server(samples_directory):
     file_server = subprocess.Popen(['fathom-serve', '-d', samples_directory], creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
     return file_server
@@ -52,7 +63,8 @@ def configure_firefox(fathom_fox, fathom_trainees, headless_browser):
     options = webdriver.FirefoxOptions()
     options.headless = headless_browser
     firefox = webdriver.Firefox(options=options)
-    firefox.install_addon(r'C:\Users\Daniel\code\fathom-fox\addon\fathom-fox.xpi', temporary=True)
+    firefox.install_addon(fathom_fox, temporary=True)
+    firefox.install_addon(fathom_trainees, temporary=True)
     return firefox
 
 
@@ -69,11 +81,11 @@ def run_vectorizer(firefox):
 
 
 def teardown(firefox, file_server):
+    firefox.quit()
     # TODO: This doesn't actually stop fathom-serve from running on my machine???
     file_server.terminate()
     file_server.kill()
     file_server.wait()
-    firefox.quit()
 
 
 #def output_vector_file(vector_file, output_directory):
