b94aa3dfd3b31005abb73fdfc3fec32764c04420
Daniel Hertenstetin <daniel.hertenstein@gmail.com>
2019-11-14 11:19:13 -0500

    Add end to end test

---
+++ cli/fathom_web/test/test_vectorize.py
@@ -0,0 +1,25 @@
+import os
+
+from click.testing import CliRunner
+
+from ..commands.vectorize import main as vectorize_main
+
+
+def test_end_to_end(tmp_path):
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    runner = CliRunner()
+    result = runner.invoke(
+        vectorize_main,
+        [
+            f'{test_dir}/resources/vectorize/vectorize_ruleset.js',
+            f'{test_dir}/resources/vectorize/',
+            os.environ['FATHOM_FOX'],
+            os.environ['FATHOM_TRAINEES'],
+            '-o',
+            f'{tmp_path.as_posix()}',
+            '-s',
+        ]
+    )
+    assert result.exit_code == 0
+    assert (tmp_path / 'vectors.json').exists()
