d6a2c0e9092b2af7ebedc43beeeac3da5b235668
Daniel Hertenstetin <daniel.hertenstein@gmail.com>
2019-11-18 15:58:51 -0500

    Try no options or profile or anything like that

--- cli/fathom_web/commands/vectorize.py
+++ cli/fathom_web/commands/vectorize.py
@@ -165,17 +165,18 @@ def configure_firefox(fathom_fox, fathom_trainees, output_directory, show_browse
     gracefully or ungracefully.
     """
     print('Configuring Firefox...', end='', flush=True)
-    options = webdriver.FirefoxOptions()
-    options.headless = not show_browser
-    profile = webdriver.FirefoxProfile()
-    print('Making profile')
-    profile.set_preference('browser.download.folderList', 2)
+#   options = webdriver.FirefoxOptions()
+#   options.headless = not show_browser
+#   profile = webdriver.FirefoxProfile()
+#   print('Making profile')
+#   profile.set_preference('browser.download.folderList', 2)
 #   profile.set_preference('browser.download.dir', str(pathlib.Path(output_directory).absolute()))
-    profile.set_preference('browser.cache.disk.enable', False)
-    profile.set_preference('browser.cache.memory.enable', False)
-    profile.set_preference('browser.cache.offline.enable', False)
+#   profile.set_preference('browser.cache.disk.enable', False)
+#   profile.set_preference('browser.cache.memory.enable', False)
+#   profile.set_preference('browser.cache.offline.enable', False)
     print('making driver')
-    firefox = webdriver.Firefox(options=options, firefox_profile=profile, service_log_path=f'{temp_dir.name}/geckodriver.log')
+#   firefox = webdriver.Firefox(options=options, firefox_profile=profile, service_log_path=f'{temp_dir.name}/geckodriver.log')
+    firefox = webdriver.Firefox()
     print('installing fathomfox')
     firefox.install_addon(fathom_fox, temporary=True)
     print('installing fathomtrainees')
