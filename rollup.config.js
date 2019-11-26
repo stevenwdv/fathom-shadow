77863d5c2db0ddc11f80aaf160a6b357b2ea1c6a
Erik Rose <erik@mozilla.com>
2019-11-25 19:05:32 -0500

    Actually, remove rollup plugins altogether, as we no longer have any npm dependencies to bundle.
    jsdom is server-side only and thus does not get bundled.

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,11 @@
 // code instead, and let rollup pull in just what Fathom code is necessary. See
 // https://github.com/mozilla/fathom-trainees/blob/master/rollup.config.js for
 // an example.
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-
 export default {
   input: 'index.mjs',
   output: {
     file: 'dist/fathom.js',
     format: 'umd',
     name: 'fathom',
-  },
-  plugins: [
-    resolve({
-      mainFields: ['main', 'jsnext:main'],
-    }),
-    commonjs({
-      include: ['node_modules/**'],
-    }),
-  ]
+  }
 };
