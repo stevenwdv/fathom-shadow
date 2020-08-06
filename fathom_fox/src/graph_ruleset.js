d4922448ddda23c75c85a866ee039f85e2ac871a
Daniel Hertenstetin <daniel.hertenstein@gmail.com>
2020-08-06 09:21:55 -0400

    Add the ruleset used for making graph based vectors

---
+++ fathom_fox/src/graph_ruleset.js
@@ -0,0 +1,67 @@
+import {element, out, rule, ruleset, score, type} from 'fathom-web';
+import {isVisible } from "fathom-web/utilsForFrontend";
+
+function tagName(fnode) {
+  return fnode.element.tagName;
+}
+
+function elementType(fnode) {
+  const type = fnode.element.type;
+  if (type === undefined) {
+    return "";
+  }
+  return type;
+}
+
+function autocompleteString(fnode) {
+  const autocomplete = fnode.element.autocomplete;
+  if (autocomplete === undefined) {
+    return "";
+  }
+  return autocomplete;
+}
+
+function makeRuleset(coeffs, biases) {
+  return ruleset([
+      rule(element("*"), type("graph")),
+      rule(type("graph"), score(tagName), {name: "tagName"}),
+      rule(type("graph"), score(elementType), {name: "elementType"}),
+      rule(type("graph"), score(isVisible), {name: "isVisible"}),
+      rule(type("graph"), score(autocompleteString), {name: "autocompleteString"}),
+      rule(type("graph"), out("graph")),
+    ],
+    coeffs,
+    biases);
+}
+
+const trainees = new Map();
+
+const VIEWPORT_SIZE = {
+  width: 1366,
+  height: 768,
+};
+
+const coefficients = [
+  ["tagName", 1.0],
+  ["elementType", 1.0],
+  ["isVisible", 1.0],
+  ["autocompleteString", 1.0]
+]
+
+const biases = [
+  ["graph", 0.0],
+]
+
+const trainee = {
+  viewportSize: VIEWPORT_SIZE,
+  coeffs: new Map(coefficients),
+  isTarget: fnode =>
+    fnode.element.dataset.fathom === "new" ||
+    fnode.element.dataset.fathom === "confirm",
+  rulesetMaker: () => makeRuleset([
+      ...coefficients],
+    biases)
+};
+trainees.set("graph", trainee);
+
+export default trainees;
