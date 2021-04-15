ff8e6481f59db2af340672dd1378ff2fde0c49eb
Erik Rose <erik@mozilla.com>
2021-04-15 11:43:07 -0400

    This doggone thing replicates the performance of our Shopping ruleset without any manual feature engineering.
    And it should run fast in Firefox because it's offthreadable. It gives 85% testing accuracy, compared with Shopping's 90% validation accuracy. (If you can't repro it, just run it again; it's not stable yet.)

---
+++ autoextract/extract.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import random
+from random import shuffle
+from sys import argv
+
+from fathom_web.utils import samples_from_dir
+from pyquery import PyQuery as pq
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score
+
+
+def main(positive_dir, negative_dir):
+    random.seed(42)
+
+    # Make sure we have the same number of positive and negative samples in
+    # training and testing. Otherwise, choose randomly, and shuffle the result
+    # so we don't throw off the first epoch. (Shuffling happens after epochs.)
+    positive_corpus = [(text_from_sample(filename), 1) for filename in samples_from_dir(positive_dir)]
+    negative_corpus = [(text_from_sample(filename), 0) for filename in samples_from_dir(negative_dir)]
+    shuffle(positive_corpus)
+    shuffle(negative_corpus)
+    TRAINING_PROPORTION = .8
+    pos_training, pos_testing = partition(positive_corpus, TRAINING_PROPORTION)
+    neg_training, neg_testing = partition(negative_corpus, TRAINING_PROPORTION)
+    training = pos_training + neg_training
+    testing = pos_testing + neg_testing
+    shuffle(training)
+    shuffle(testing)
+
+    # Unzip xs and ys:
+    x_train, y_train = zip(*training)
+    x_test, y_test = zip(*testing)
+
+    # These settings seem to nicely weight heavily words that are uncommon
+    # across docs and demote (though not eliminate) stopwords:
+    vectorizer = TfidfVectorizer(max_df=.8)  # Decrease max_df to be more aggressive about declaring things stopwords.
+    # Learn the words in the corpus. Other words seen in the future will be
+    # ignored. Automatically reasons out stopwords by default.
+    tfidf_matrix = vectorizer.fit_transform(x_train)
+    #print(tfidf_matrix.toarray())
+    # TODO: Add stemming. Otherwise evaluate the tokenizer.
+    print('Stopwords:', vectorizer.stop_words_)
+    #print(vectorizer.vocabulary_)
+    # TODO: Consider using a hashing vectorizer in production to save
+    # RAM/storage with large vocabs.
+
+    x_train = vectorizer.fit_transform(x_train)
+    x_test = vectorizer.transform(x_test)
+
+    # Chi^2 seems like a weird thing to use for non-categoricals.
+    #     ch2 = SelectKBest(chi2, k=1000)
+    #     x_train = ch2.fit_transform(x_train, y_train)
+    #     x_test = ch2.transform(x_test)
+
+    # price regex as signal? But I don't want to write signal.
+
+    classifier = PassiveAggressiveClassifier(early_stopping=True, class_weight='balanced', validation_fraction=.2)
+    classifier.fit(x_train, y_train)
+    pred = classifier.predict(x_test)
+    accuracy = accuracy_score(y_test, pred)
+    print(f'Accuracy: {accuracy}')
+
+
+def partition(sliceable, proportion):
+    """Divide sliceable into 2 pieces, and return them as a tuple."""
+    boundary = round(proportion * len(sliceable))
+    return sliceable[:boundary], sliceable[boundary:]
+
+
+def text_from_sample(filename):
+    """Return the innerText (or thereabouts) from an HTML file."""
+    with open(filename, encoding='utf-8') as file:
+        return pq(file.read()).text()
+
+
+if __name__ == '__main__':
+    main(argv[1], argv[2])
