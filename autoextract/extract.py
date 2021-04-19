a9fdd20d9c72c8245da372a58c79b1b736455497
Erik Rose <erik@mozilla.com>
2021-04-19 12:28:11 -0400

    Add tensorboard graphs so I can see what's going on, toward getting a consistent final result.

--- autoextract/extract.py
+++ autoextract/extract.py
@@ -4,12 +4,15 @@ import random
 from random import shuffle
 from sys import argv
 
+from click import progressbar
 from fathom_web.utils import samples_from_dir
+from numpy import unique
 from pyquery import PyQuery as pq
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
-from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import PassiveAggressiveClassifier, SGDClassifier
+from sklearn.metrics import accuracy_score, hinge_loss, log_loss
+from tensorboardX import SummaryWriter
 
 
 def load_data(positive_dir, negative_dir):
@@ -21,7 +24,7 @@ def load_data(positive_dir, negative_dir):
 
     """
     positive_corpus = [(text_from_sample(filename), 1) for filename in samples_from_dir(positive_dir)]
-    negative_corpus = [(text_from_sample(filename), 0) for filename in samples_from_dir(negative_dir)]
+    negative_corpus = [(text_from_sample(filename), -1) for filename in samples_from_dir(negative_dir)]
     shuffle(positive_corpus)
     shuffle(negative_corpus)
     TRAINING_PROPORTION = .8
@@ -46,15 +49,15 @@ def vectorize_data(x_train, x_test):
     vectorizer = TfidfVectorizer(max_df=.8)  # Decrease max_df to be more aggressive about declaring things stopwords.
     # Learn the words in the corpus. Other words seen in the future will be
     # ignored. Automatically reasons out stopwords by default.
-    tfidf_matrix = vectorizer.fit_transform(x_train)
+    #tfidf_matrix = vectorizer.fit_transform(x_train)
     #print(tfidf_matrix.toarray())
     # TODO: Add stemming. Otherwise evaluate the tokenizer.
-    print('Stopwords:', vectorizer.stop_words_)
     #print(vectorizer.vocabulary_)
     # TODO: Consider using a hashing vectorizer in production to save
     # RAM/storage with large vocabs.
 
     x_train = vectorizer.fit_transform(x_train)
+    print('Stopwords:', vectorizer.stop_words_)
     x_test = vectorizer.transform(x_test)
 
     # Chi^2 seems like a weird thing to use for non-categoricals.
@@ -67,10 +70,49 @@ def vectorize_data(x_train, x_test):
     return x_train, x_test
 
 
-def model_data(x_train, y_train, x_test, y_test):
+def model_data(x_train, y_train, iterations, validation_fraction=0.2):
     """Train and return a predictive model of the data."""
-    model = PassiveAggressiveClassifier(early_stopping=True, class_weight='balanced', validation_fraction=.2)
-    model.fit(x_train, y_train)
+    #model = PassiveAggressiveClassifier()
+    # early_stopping=True, class_weight='balanced', validation_fraction=.2
+    #loss_fn = hinge_loss
+    model = SGDClassifier(loss='log', eta0=.01, learning_rate='constant', penalty=None)
+    loss_fn = log_loss
+    x_validation, x_train = partition(x_train, validation_fraction)
+    y_validation, y_train = partition(y_train, validation_fraction)
+    writer = SummaryWriter(comment='sgd log, eta0=.001')
+
+    classes = unique(y_train)
+    # TODO: Balance classes.
+    prev_validation_loss = None
+    with progressbar(range(iterations), label='Training') as bar:
+        for t in bar:
+            model.partial_fit(x_train, y_train, classes=classes)
+            pred = model.predict(x_train)
+            accuracy = accuracy_score(y_train, pred)
+            validation_loss = loss_fn(y_validation, model.predict(x_validation))
+            writer.add_scalar('training accuracy',
+                              accuracy,
+                              t)
+            writer.add_scalar('training loss',
+                              loss_fn(y_train, pred),
+                              t)
+            writer.add_scalar('validation loss',
+                              validation_loss,
+                              t)
+
+            if False:
+                # Do early stopping:
+                if prev_validation_loss is not None and validation_loss > prev_validation_loss and t >= 13:
+                    # Restore previous model:
+                    model.coef_ = prev_coef
+                    model.intercept_ = prev_intercept
+                    print(f'Stopping early at iteration {t} with training accuracy {accuracy}.')
+                    break
+                else:
+                    prev_validation_loss = validation_loss
+                    # TODO: Don't redo allocation each time.
+                    prev_coef = model.coef_.copy()
+                    prev_intercept = model.intercept_.copy()  # copy() not really needed
     return model
 
 
@@ -80,7 +122,7 @@ def main(positive_dir, negative_dir):
     x_train, y_train, x_test, y_test = load_data(positive_dir, negative_dir)
     x_train, x_test = vectorize_data(x_train, x_test)
     print(f'Dimensions: {x_train.shape[1]}')
-    model = model_data(x_train, y_train, x_test, y_test)
+    model = model_data(x_train, y_train, 1500)
     pred = model.predict(x_test)
     accuracy = accuracy_score(y_test, pred)
     print(f'Accuracy: {accuracy}')
@@ -90,7 +132,11 @@ def main(positive_dir, negative_dir):
 
 def partition(sliceable, proportion):
     """Divide sliceable into 2 pieces, and return them as a tuple."""
-    boundary = round(proportion * len(sliceable))
+    try:
+        length = len(sliceable)
+    except TypeError:  # It was a matrix.
+        length = len(sliceable.getnnz(1))
+    boundary = round(proportion * length)
     return sliceable[:boundary], sliceable[boundary:]
 
 
